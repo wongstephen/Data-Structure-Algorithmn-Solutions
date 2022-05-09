f8482c2aa2bbd81c83150402b6be11c5a890786a
Stephen Wong <wongstephenky@gmail.com>
2022-05-08 17:34:52 -0700

    added solution 3

---
+++ js/solution003.js
@@ -0,0 +1,13 @@
+//Convert number to reversed array of digits
+function digitize(n) {
+    //code here
+  
+  //   console.log(n) // 35231
+     const output = [];
+     const number = n.toString();
+     const arr = number.split('');
+     for (let i = arr.length-1; i >= 0; i--) {
+        output.push(Number(arr[i]));
+     }
+    return output
+  }
