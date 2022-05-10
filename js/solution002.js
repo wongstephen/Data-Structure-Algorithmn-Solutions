b49108df414821618e3b7c4f428a976cac784d7e
Stephen Wong <wongstephenky@gmail.com>
2022-05-09 17:02:28 -0700

    completed solution 002

---
+++ js/solution002.js
@@ -0,0 +1,15 @@
+function solution(number){
+    const arr = []
+    for (let i = 0; i < number; i++) {
+      if (i%15 === 0) {
+        arr.push(i);
+      } else if (i%5 === 0) {
+        arr.push(i);
+      } else if (i%3 === 0) {
+        arr.push(i);
+      }
+    }
+    const sumArr = arr.reduce(function (a, c) {
+      return a + c},0)
+    return sumArr
+  }
