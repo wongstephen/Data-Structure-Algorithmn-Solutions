a8a44e9486ed0908db82607116da0723eee24b95
Stephen Wong <wongstephenky@gmail.com>
2022-05-10 17:30:26 -0700

    redo 8

--- js/solution008.js
+++ js/solution008.js
@@ -1,25 +1,9 @@
-/* describe("Tests", () => {
-    it("test", () => {
-  Test.assertSimilar(deleteNth([20,37,20,21], 1), [20,37,21])
-  Test.assertSimilar(deleteNth([1,1,3,3,7,2,2,2,2], 3), [1, 1, 3, 3, 7, 2, 2, 2])
-  
-    });
-  });
-   */
+//factorial
 
-  function deleteNth(arr,n){
-/*     console.log(arr.filter(e=>e===20).length) //number of occurancees
-    console.log(arr.lastIndexOf(20))
-    console.log(arr)
-    arr.splice(arr.lastIndexOf(20),1)
-    console.log(arr) */
-    arr.forEach(e=>{
-        const countE = arr.filter(i=>i===e).length
-        for (let i = 0; i < ; i++) {
-            arr.splice(arr.lastIndexOf(e),1)
-        }
-    })
-    console.log(arr)
-  }
-
-  deleteNth([20,37,20,21], 1)
+function factorial(n){
+    if (n==1 || n ==0){
+      return 1
+    } else {
+      return n * factorial(n-1);
+    }
+}
