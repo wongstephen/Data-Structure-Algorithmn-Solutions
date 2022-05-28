9e4aaaedb110f7d82eb100eac5ac6b76a27a14e5
wongstephen <wongstephenky@gmail.com>
2022-05-27 21:34:38 -0700

    more stuff

---
+++ js/test.js
@@ -0,0 +1,14 @@
+const tags = ['wrong', 'spot', 'dumbass']
+
+const video = {
+    title: 'Super Bad',
+    tags: ['a', 'b', 'c', 'd'],
+    play() {
+        this.tags.forEach(function(element) {
+            console.log(this, element)
+        }, this.title)
+    } //this is a function in a obj, thus called and method
+} // this is an object
+video.play()
+ 
+
