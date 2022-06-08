446bf83af1df739afec628ea20325dd4e67d6f63
wongstephen <wongstephenky@gmail.com>
2022-06-07 20:33:35 -0700

    solution032.js

--- js/solution031.js
+++ js/solution031.js
@@ -18,3 +18,19 @@ function vowelIndices(word){
 	}
 	return output  
   }
+
+
+/* tests
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold = 0;
+
+describe("Basic tests", function(){
+  it("Test", () => {
+    assert.deepEqual(vowelIndices("mmm"), []);
+    assert.deepEqual(vowelIndices("apple"), [1,5]);
+    assert.deepEqual(vowelIndices("super"), [2,4]);
+    assert.deepEqual(vowelIndices("orange"), [1,3,6]);
+    assert.deepEqual(vowelIndices("supercalifragilisticexpialidocious"), [2,4,7,9,12,14,16,19,21,24,25,27,29,31,32,33]);
+  })
+}); */
