167c8ff95354395ae9ff00a2296e15879ec1fcbb
Stephen Wong <wongstephenky@gmail.com>
2022-07-02 19:07:56 -0700

    sol 45

---
+++ js/solution045-string-to-camel.js
@@ -0,0 +1,14 @@
+function toCamelCase(str){
+  let output = []
+  if (str == '') {return}
+  if(str.indexOf('-')>0) {
+    str.toLowerCase().split('-')
+    output = str.pop()
+    console.log(output)
+    console.log(str)
+  }
+  if (str.indexOf('_')>0) {
+    n
+  }
+
+}
