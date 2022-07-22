38ef677cb1b9fef4a617781e87a5300210102830
Stephen Wong <wongstephenky@gmail.com>
2022-07-22 09:22:45 -0700

    sol 56

---
+++ js/solution056-fake-binary.js
@@ -0,0 +1,7 @@
+function fakeBin(x){
+  return x.split('').map(el=>{return el<5?0:1}).join('')
+}
+
+//Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+// Note: input will never be an empty string
