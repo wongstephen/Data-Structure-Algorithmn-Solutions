e6cd2fb465410823fefdfdef1f3688be21e9899d
Stephen Wong <wongstephenky@gmail.com>
2022-10-09 09:32:38 -0700

    sol92

---
+++ js/solution094-best-stock-cw.py
@@ -0,0 +1,15 @@
+#([10, 7, 5, 8, 11, 9]), 6)
+#. l   r
+def max_profit(prices):
+    l = 0
+    r = 1
+    max = -1
+    while r < len(prices):
+        if prices[l]<=prices[r]:
+            diff = prices[r]-prices[l]
+            if max < diff:
+                max = diff
+        else:
+            l = r
+        r = r + 1
+    return max
