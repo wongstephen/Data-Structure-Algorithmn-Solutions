27be8747dac5c5979064b8f89844234bf864b4fb
Stephen Wong <wongstephenky@gmail.com>
2023-01-10 22:42:09 -0800

    updated choc

--- js/java/Chocolate.java
+++ js/java/Chocolate.java
@@ -1,7 +1,7 @@
-package java;
 public class Chocolate{
     public static long breakChocolate(long n, long m) {
-    
       return n * m - 1;
     }
   }
+
+// returns how many choc in a squre grid
