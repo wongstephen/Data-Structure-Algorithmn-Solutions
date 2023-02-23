31add42b24075f1e2248401ecd85a3f628566f51
Stephen Wong <wongstephenky@gmail.com>
2023-02-23 14:04:34 -0800

    sid

---
+++ java/simpleMultiplication.java
@@ -0,0 +1,9 @@
+public class Sid {
+    public static int simpleMultiplication(int n) {
+        if (n % 2 == 0) {
+          return n * 8;
+        } else {
+          return n * 9;
+        }
+    }
+}
